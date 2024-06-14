114a4e784698b612585ce0d0fb284e1ab6e13caa
Nikita Blizniuk <bliznyuknm@gmail.com>
2024-06-15 01:44:33 +0200

    smoothed mask borders

--- buildings/building.gd
+++ buildings/building.gd
@@ -12,7 +12,7 @@ func enter_floor(floor_number: int) -> void:
         var _floor: Node2D = floors.get_child(i)
         var interior: BackBufferCopy = _floor.get_node("Interior")
         interior.copy_mode = BackBufferCopy.CopyMode.COPY_MODE_DISABLED if i != floor_number else BackBufferCopy.CopyMode.COPY_MODE_VIEWPORT
-        interior.get_node("Floor").z_index = 0 if i != floor_number else -1
+        interior.get_node("Floor").z_index = 0 if floor_number >= 0 and i != floor_number else -1
         _floor.get_node("InteriorMask").visible = i == floor_number
         _floor.get_node("ExternalWalls").collision_enabled = i != floor_number
         #_floor.get_node("Roof").toggle(floor_number != floors.get_child_count() - 1)
