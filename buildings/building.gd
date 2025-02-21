0f11e104889d7565b5fd8b98ddcd476932a20893
Nikita Blizniuk <bliznyuknm@gmail.com>
2025-02-21 10:43:48 +0100

    different rooms works good, floors WIP

--- buildings/building.gd
+++ buildings/building.gd
@@ -4,20 +4,25 @@ extends Node2D
 @onready var floors: = $Floors
 
 
-const FLOOR_OUTSIDE: = -1
+const OUTSIDE: = -1
 
 
-func _ready() -> void: enter_floor(FLOOR_OUTSIDE)
+func _ready() -> void: enter_room(OUTSIDE, OUTSIDE)
 
 
-func enter_floor(floor_number: int) -> void:
-	for i in floors.get_child_count():
-		var _floor: Node2D = floors.get_child(i)
-		var interior: BackBufferCopy = _floor.get_node("Interior")
-		interior.copy_mode = BackBufferCopy.CopyMode.COPY_MODE_DISABLED if i != floor_number else BackBufferCopy.CopyMode.COPY_MODE_VIEWPORT
-		interior.get_node("Floor").z_index = 0 if floor_number >= 0 and i != floor_number else -1
-		_floor.get_node("InteriorMask").visible = i == floor_number
-		_floor.get_node("ExternalWalls").collision_enabled = i != floor_number
+func enter_room(floor_id: int, room_id: int) -> void:
+	for floor_index in floors.get_child_count():
+		var floor: Node2D = floors.get_child(floor_index)
+		
+		for room_index in floor.get_child_count():
+			var exact_room: = floor_id == floor_index and room_id == room_index
+			var room: Node2D = floor.get_child(room_index)
+			var interior: BackBufferCopy = room.get_node("Interior")
+			interior.copy_mode = BackBufferCopy.CopyMode.COPY_MODE_DISABLED if not exact_room else BackBufferCopy.CopyMode.COPY_MODE_VIEWPORT
+			interior.get_node("Floor").z_index = 0 if floor_id >= 0 and not exact_room else -1
+			room.get_node("InteriorMask").visible = exact_room
+			room.get_node("ExternalWalls").collision_enabled = floor_id != floor_index
+			room.z_index = 0 if exact_room else -2
 		#_floor.get_node("Roof").toggle(floor_number != floors.get_child_count() - 1)
 		
 		#if floor_number >= 0 and i - 1 == floor_number: _floor.modulate = Color(1, 1, 1, 0.2)
--- buildings/entrance.gd
+++ buildings/entrance.gd
@@ -3,12 +3,12 @@ extends Area2D
 
 @export_enum("Up:1", "Down:-1") var direction: int
 @export var floor_id: int = -1 # negative means exiting from the building
-@export var root: Node
+@export var room_id: int = -1 # negative means exiting from the building
 
 
 @onready var entrance_point: = $EntrancePoint
 
 
 func enter(node: Node2D) -> void:
-	owner.enter_floor(floor_id)
+	owner.enter_room(floor_id, room_id)
 	node.global_position = entrance_point.global_position
